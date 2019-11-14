Correct RDF volume normalization re Jean-Marc code

utils.py
@@ -205,16 +205,20 @@ class Traj(object):
                            rdfs[pair][int(dist[step]/binwidth)] += 1
             if (iatom % 1000) == 0:
                 print "For atom %s finish in:" % iatom, time.time() - start
-        vol = np.zeros(nbins)
-        for i, rr in enumerate(bins):
+        vol = np.zeros(nbins+1)
+        for i, rr in enumerate(np.append(bins,bins[-1]+binwidth)):
             vol[i] = ((4.0/3.0) * np.pi ) * rr**3
             if rr > self.box_length / 2:
                 x = self.box_length / (2 * rr)
                 vol[i] = vol[i] * ( - 2 + 4.5*x  - 1.5 * x**3)
         for pair in pairs:
+            dens = self.count_pair(pair) / self.box_length ** 3
+            histo_id = np.zeros(nbins)
+            for i in range(1, nbins+1):
+                histo_id[i-1] = dens*(vol[i] - vol[i-1])
             for i in range(1, nbins):
-                rdfs[pair][i] = rdfs[pair][i] / (vol[i] - vol[i-1])
-            rdfs[pair] =  ( 1 + int(pair[0] == pair[1]) )* rdfs[pair] * self.box_length ** 3 / (self.count_pair(pair) * self.steps)
+                rdfs[pair][i] = rdfs[pair][i] / histo_id[i]
+            rdfs[pair] =  ( 1 + int(pair[0] == pair[1]) )* rdfs[pair] / self.steps
         self.bins = bins
         self.rdfs = rdfs
         print "Total time is :", time.time() - start_tot